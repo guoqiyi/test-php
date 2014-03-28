a1875032b455b21b0649f1ed2cdab311734053b1
guoqiyi <guoqiyi@qq.com>
2014-03-29 07:58:49 +0800

    Initial commit

---
+++ Car.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: yi
+ * Date: 14-3-10
+ * Time: 下午5:12
+ */
+class Car
+{
+    public $color;
+    public $price;
+    private $master;
+}
+
+?>
---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: yi
+ * Date: 14-3-10
+ * Time: 下午5:14
+ */
+echo(getcwd());
+?>
