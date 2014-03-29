7063080af1cf09fe4335e1dbcc91e252b5e6b400
guoqiyi <guoqiyi@qq.com>
2014-03-29 08:20:26 +0800

    change test.php

--- test.php
+++ test.php
@@ -8,6 +8,7 @@ echo "'$a'" . "<br>";
 echo '"$a"' . "<br>";
 echo PHP_OS . "<br>";
 echo __FILE__ . "<br>";
-echo __DIR__;
-echo(phpversion());
+echo __DIR__. "<br>";
+echo("PHP Version is ".phpversion().".<br>");
+echo("PHP Version is ".PHP_VERSION).".";
 ?>
