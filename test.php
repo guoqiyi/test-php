3b65569933d5aba246ef485fd0581fdab24c6fa3
guoqiyi <guoqiyi@qq.com>
2014-03-29 08:25:15 +0800

    change test.php again

--- test.php
+++ test.php
@@ -10,5 +10,6 @@ echo PHP_OS . "<br>";
 echo __FILE__ . "<br>";
 echo __DIR__. "<br>";
 echo("PHP Version is ".phpversion().".<br>");
-echo("PHP Version is ".PHP_VERSION).".";
+echo("PHP Version is ".PHP_VERSION).".<br>";
+echo("PHP's configure file is ".php_ini_loaded_file()).".";
 ?>
