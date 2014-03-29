419f17b4b86e94ebee3692c589fc98a1fe129764
guoqiyi <guoqiyi@qq.com>
2014-03-29 08:33:23 +0800

    add 3 files

---
+++ cwd.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: yi
+ * Date: 14-3-1
+ * Time: 下午10:49
+ */
+echo(getcwd())."<br />";
+echo(__DIR__)."<br />";
+echo(__FILE__)."<br />";
+echo(PHP_DATADIR)."<br />";
+echo(PHP_OS)."<br />";
+echo(PHP_LIBDIR)."<br />";
+echo(PHP_CONFIG_FILE_PATH)."<br />";
+echo(PHP_CONFIG_FILE_SCAN_DIR)."<br />";
+echo(PHP_MANDIR)."<br />";
+echo(PHP_LOCALSTATEDIR)."<br />";
+echo(__LINE__)."<br />";
+?>
---
+++ html.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8"/>
+    <title>HTML5</title>
+</head>
+<body>
+<header>
+    <nav>
+        <?php
+        echo "<ul><li>One</li><li>Two</li><li>Three</li></ul>"
+        ?>
+    </nav>
+</header>
+<footer>
+
+</footer>
+</body>
+</html>
