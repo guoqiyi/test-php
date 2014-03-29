a1a196723b8c868db3341fc061f5c9d990f11212
guoqiyi <guoqiyi@qq.com>
2014-03-29 08:15:07 +0800

    add test.php

---
+++ test.php
@@ -0,0 +1,13 @@
+<?php
+//phpinfo();
+$a = "arch";
+echo $a . "<br>";
+echo '$a' . "<br>";
+echo "$a" . "<br>";
+echo "'$a'" . "<br>";
+echo '"$a"' . "<br>";
+echo PHP_OS . "<br>";
+echo __FILE__ . "<br>";
+echo __DIR__;
+echo(phpversion());
+?>
